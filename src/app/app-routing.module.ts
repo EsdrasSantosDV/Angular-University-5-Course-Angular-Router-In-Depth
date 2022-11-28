f819367c078ce2983865c4e85181ca23e59dcf00
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-27 22:03:33 -0300

    feat:page route not found, main route, route order, patchmatch, redirectTo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,27 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {AboutComponent} from './about/about.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+
+
+  //IMPORTANTE A ORDEM DAS RTOAS IMPORTA, NUNCA COLOCAR A ROTA DO PAGE NOT FOUND
+  //QUE É  ** DE PRIMEIRA, SENÃO QUALQEUR ROTA VAI DAR NOT FOUND
+
+
+
+ //A  ORDEM IMPORTA PRO PAGE NOT FOUND
+  //PQ ELE GENERALIZA
+  {
+    // E O "" E PREFIXO DE QUALQUER OUTRO CAMINHO
+    path:"",
+    //POR ISSO PRECISAMOS DO PATH MATCH full
+    pathMatch:"full",
+    //REDIRECIONAMOS PRA /Courses
+    redirectTo:"/courses"
+  },
   {
     path:"courses",
     //CARREGAMENTO PREGUIÇOSO LAZY LOADING JEITO IDEAL
@@ -23,8 +41,14 @@ const routes: Routes = [
   {
     path:"about",
     component:AboutComponent
-  }
-
+  },
+  {
+    //PAGINA NÃO ENCONTRADA
+    //PATH PEGA TODAS QUE NÃO TA MAPEADA AQUI
+    //NUNCA COLOCAR A ROTA DO PAGE NOT FOUND COMO PRIMEIRA
+    path:"**",
+    component:PageNotFoundComponent
+  },
 
 
 
