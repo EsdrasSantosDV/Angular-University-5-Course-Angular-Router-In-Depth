a7cf596283ea1ca302f3969831609d5df9cb7d75
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-28 19:37:43 -0300

    feat:router outlet

--- src/app/testerotasstand/principal/principal.component.ts
+++ src/app/testerotasstand/principal/principal.component.ts
@@ -1,13 +1,14 @@
 
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core'
+import { RouterModule } from '@angular/router';
 
 
 
 @Component({
   selector: 'principal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.scss']
 })
