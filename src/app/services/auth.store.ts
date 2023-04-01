81a5bd66807ceb88e4106e371c1be7e25e202a19
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-04-01 17:24:16 -0300

    feat:adding auth store subject

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -11,19 +11,29 @@ const AUTH_DATA = "auth_data";
 })
 export class AuthStore {
 
+    //TEMOS AQUI UMA STORE DE AUTENTICAÇÃO
+    //PQ ELA ARMAZENA O ESTADO DO USUARIO LOGADO
+
+
+    //AQUI EXPOMOS PRA TODA A APLICAÇÃO O NOSSO OBSERVAVEL
     private subject = new BehaviorSubject<User>(null);
+    //QUALQUER PARTE DA APLICAÇÃO PODE SE INSCREVER NESSE OBSERVAVEL
 
     user$ : Observable<User> = this.subject.asObservable();
 
+    //TEMOS AQUI NOSSOS DOIS ESTADOS
     isLoggedIn$ : Observable<boolean>;
     isLoggedOut$ : Observable<boolean>;
 
     constructor(private http: HttpClient) {
-
+        //PODEMOS VER QUE ELES SÃO ORIUNDOS DO NOSSO OBSERVABLE
+        //DO USUARIO
         this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
-
+        //SE TIVER UM PERFIL DE USUARIO PRA EMITIR
+        //ELE VAI GUARDAR O ESTADO DE LOGIN E LOGOUT
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
 
+        
         const user = localStorage.getItem(AUTH_DATA);
 
         if (user) {
